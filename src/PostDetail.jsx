ad10619efaeed6d4fa8b1cf6a0b19e9a8ba1d32d
polly.j <polly.j@pollyjui-MacBookPro.local>
2022-09-21 17:00:31 +0900

    Mutations(변이) 사용

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useQuery, useMutation } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -24,16 +24,21 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  const { data, isError, error, isLoading } = useQuery(["comments", post.id], ()=> fetchComments(post.id));
-  
-  if(isLoading) return <div>로딩중...</div>
+  const { data, isError, error, isLoading } = useQuery(["comments", post.id], () => fetchComments(post.id));
 
-  if(isError) return <div>에러..!!</div>
+  const deleteMutation = useMutation((postId) => deletePost(postId))
+
+  if (isLoading) return <div>로딩중...</div>
+
+  if (isError) return <div>에러..!!</div>
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button> <button>Update title</button>
+      {deleteMutation.isError && <p style={{ color: 'red' }}>Error deleting the post</p>}
+      {deleteMutation.isLoading && <p style={{ color: 'purple' }}>Loading deleting the post</p>}
+      {deleteMutation.isSuccess && <p style={{ color: 'green' }}>Success deleting the post</p>}
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data.map((comment) => (

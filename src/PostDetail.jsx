acf397a3d2fb86001cd183a5e708d84e9687851b
polly.j <polly.j@pollyjui-MacBookPro.local>
2022-09-21 12:22:25 +0900

    코멘트 리액트 쿼리로  fetch 받기

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -22,8 +24,11 @@ async function updatePost(postId) {
 }
 
 export function PostDetail({ post }) {
-  // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery(["comments", post.id], ()=> fetchComments(post.id));
+  
+  if(isLoading) return <div>로딩중...</div>
+
+  if(isError) return <div>에러..!!</div>
 
   return (
     <>
